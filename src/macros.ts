be65d62baa93c0e0105e5923902debca4174bed3
Freek Pols <55780866+FreekPols@users.noreply.github.com>
2025-10-23 16:03:39 +0200

    Add vartheta macro to macros.ts

--- src/macros.ts
+++ src/macros.ts
@@ -68,6 +68,7 @@ export const typstMacros: Record<string, string | ((state: IState, node: LatexNo
   oint: 'integral.cont',
   oiint: 'integral.surf',
   oiiint: 'integral.vol',
+  vartheta: 'theta.alt',
   sqrt: (state, node) => {
     if (isEmptyNode(node.args?.[0])) return 'sqrt';
     return 'root';
